9280f8386bed97bb1229ba77424c79017addbe91
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-12-04 08:23:59 +0530

    Added Module design_patterns

---
+++ design_patterns/src/main/java/OverallDesignPatterns.java
@@ -0,0 +1,6 @@
+public class OverallDesignPatterns {
+    public static void main(String[] args) {
+        System.out.println("Working with All Design Patterns, " +
+                "will segregate later into \n1. Creational \n2. Structural \n3. Behavioural");
+    }
+}
