4538d5f3ca65cb80492b04d3913e74b4b74fdb5a
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-12-08 09:04:55 +0530

    Added builder design pattern ending code

---
+++ design_patterns/src/main/java/creational_patterns/builder/end/UserDTO.java
@@ -0,0 +1,11 @@
+package creational_patterns.builder.end;
+
+//Interface implemented by "products"
+public interface UserDTO {
+
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+}
---
+++ design_patterns/src/main/java/creational_patterns/builder/start/UserDTO.java
@@ -0,0 +1,11 @@
+package creational_patterns.builder.start;
+
+//Interface implemented by "products"
+public interface UserDTO {
+	
+	String getName();
+
+	String getAddress();
+
+	String getAge();
+}
