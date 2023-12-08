0e59bcd133c6bcb594660d5ce9764f8e4110b0ea
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-12-08 09:04:22 +0530

    Added builder design pattern implementation 1 code

---
+++ design_patterns/src/main/java/creational_patterns/builder/inbetween/builder_impl1/UserDTO.java
@@ -0,0 +1,11 @@
+package creational_patterns.builder.inbetween.builder_impl1;
+
+//Interface implemented by "products"
+public interface UserDTO {
+
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+}
---
+++ design_patterns/src/main/java/creational_patterns/builder/inbetween/builder_impl1/UserDTOBuilder.java
@@ -0,0 +1,24 @@
+package creational_patterns.builder.inbetween.builder_impl1;
+
+import creational_patterns.builder.models.Address;
+
+import java.time.LocalDate;
+
+//Abstract builder
+public interface UserDTOBuilder {
+    //methods to build "parts" of product at a time
+    UserDTOBuilder withFirstName(String fname);
+
+    UserDTOBuilder withLastName(String lname);
+
+    UserDTOBuilder withBirthday(LocalDate date);
+
+    UserDTOBuilder withAddress(Address address);
+
+    //method to "assemble" final product
+    UserDTO build();
+
+    //(optional) method to fetch already built object
+    UserDTO getUserDTO();
+}
+
